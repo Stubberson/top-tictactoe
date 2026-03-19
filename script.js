65d525184fb165d856dafb339714e2550ab7bb0a
Stubberson <tuukka.j.korpela@gmail.com>
2026-03-19 14:51:48 +0100

    Add indicator for placed tic or tac

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const Gameboard = (() => {
-    const grid = document.querySelector('#board')
-    return {grid}
-})()
-
-const GameController = (() => {
-    let memory = []
-    Gameboard.grid.addEventListener('click', (event) => {
-        memory.push(event.target.id)
-        event.target.innerText = 'clicked'
-    })
+    const cells = document.querySelectorAll('.cell')
+    return {cells}
 })()
 
 function CreatePlayer(name) {
@@ -17,3 +9,21 @@ function CreatePlayer(name) {
         return {name, greeting}
     }
 }
+
+const GameController = (() => {
+    let memory = []
+    for (let cell of Gameboard.cells) {
+        cell.addEventListener('click', (event) => {
+            if (!memory.includes(event.target)) {
+                memory.push(event.target)
+                if (memory.length % 2 !== 0) {
+                    event.target.classList.add('player1')
+                    event.target.style.backgroundColor = 'red'
+                } else {
+                    event.target.classList.add('player2')
+                    event.target.style.backgroundColor = 'blue'
+                }
+            }
+        })
+    }
+})()
